f6660567b148b2f76df332e4d700a0af8bafc4b3
Pawel Mozola <pawel.mozola@aerdata.com>
2021-05-14 21:57:17 +0200

    reorder methods

--- API/BookSharing/Auth/BookSharing.Auth.Application/Results/Result.cs
+++ API/BookSharing/Auth/BookSharing.Auth.Application/Results/Result.cs
@@ -47,9 +47,6 @@ namespace BookSharing.Auth.Application.Results
         public TResult Match<TResult>(Func<T, TResult> success, Func<Exception, TResult> error)
             => IsSuccessful ? success(Data) : error(Exception);
 
-        public Task<TResult> MatchAsync<TResult>(Func<T, Task<TResult>> success, Func<Exception, TResult> error)
-            => IsSuccessful ? success(Data) : Task.FromResult(error(Exception));
-
         public void Match(Action<T> success, Action error)
         {
             if (IsSuccessful) success(Data);
@@ -62,6 +59,9 @@ namespace BookSharing.Auth.Application.Results
             else error(Exception);
         }
 
+        public Task<TResult> MatchAsync<TResult>(Func<T, Task<TResult>> success, Func<Exception, TResult> error)
+            => IsSuccessful ? success(Data) : Task.FromResult(error(Exception));
+        
         public static Result<T> Success(T data) => new(data);
         public new static Result<T> Error(Exception error) => new(error);
     }
