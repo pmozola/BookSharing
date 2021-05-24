e721e2fb30a6d27ee37bc35131c78c92456a612b
pmozola <mozola.pawel@gmail.com>
2021-05-24 16:39:35 +0200

    add load interceptor (#21)
    Co-authored-by: Pawel Mozola <pawel.mozola@aerdata.com>

--- Mobile/bookSharing/src/app/app.module.ts
+++ Mobile/bookSharing/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +20,8 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }],
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
